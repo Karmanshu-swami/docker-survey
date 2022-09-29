27009018707b2df4425b49343925d15d2af0817e
karmanshu <swami.karmanshu1398@gmail.com>
2022-09-29 16:36:14 +0530

    v2 of docker-survey

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended:false}));
+const dotenv = require('dotenv').config();
 const frontendrouter = require('./router/frontend')
-const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://admin:Admin123@13.233.89.198:27017/?tls=false&authMechanism=DEFAULT/studentrec', ()=>{
-//     console.log("Connected to database successfully!");
-// });
+// Initialize DB
+const initDB = require('./initDB');
+initDB();
 
-mongoose.connect('mongodb://127.0.0.1:27017/studentrec' , ()=>{
-    console.log("Connected to database successfully!!");
-})
-
-const Port = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(frontendrouter);
@@ -22,6 +18,6 @@ app.set("view engine", "ejs")
 
 
 
-app.listen(Port, ()=>{
+app.listen(PORT, ()=>{
     console.log("Listening to port 5000");
 });
